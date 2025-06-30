d7078a01030823fa7b7d22e8d1fb549ec4ce3e16
Leonardo Silva e Cruz <101243664+HiperD@users.noreply.github.com>
2025-06-30 15:23:50 -0300

    teste 1

---
+++ script.js
@@ -0,0 +1,18 @@
+alert('Seja bem-vindo a calculadora de notas!');
+
+qtdNotas = parseInt(prompt('Digite a quantodade de Notas que compóem a média'));
+
+do {
+    let media = 0;
+    for (let i = 0; i < qtdNotas; i++) {
+        media = media + parseInt(prompt(`Digite ${i}º nota`));
+    }
+    media = media/qtdNotas;
+    if(media >=6){
+        alert(`Aluno foi aprovado com media final ${media}`);
+    }
+    else{
+        alert(`Aluno foi reprovado com media final ${media}`);
+    }
+} while (condition);
+
